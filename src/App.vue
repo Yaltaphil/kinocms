a6d93d56e56996e69be031320452078c6991c4bd
Yaltaphil <yaltaphil@yandex.ru>
2021-07-12 19:03:11 +0300

    admin-lte connected

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link to="/news">News</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+            <router-link to="/news">News</router-link>
+        </div>
+        <transition name="fade"> <router-view /> </transition>>
     </div>
-    <transition name="fade"> <router-view /> </transition>>
-  </div>
 </template>
 
 <style lang="scss">
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 1s;
+    transition: opacity 1s;
 }
 .fade-enter,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+//admin-lte
+import 'admin-lte/plugins/jquery/jquery.min.js'
+import 'admin-lte/plugins/bootstrap/js/bootstrap.min.js'
+import 'admin-lte/dist/css/adminlte.min.css'
+import 'admin-lte/dist/js/adminlte.min.js'
+import 'admin-lte/plugins/fontawesome-free/css/all.min.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
