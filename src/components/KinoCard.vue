515b90baebd7b7189aeb5b8cfde82b2d133661ae
Yaltaphil <yaltaphil@gmail.com>
2021-07-17 22:50:42 +0300

    add: banners and kino-card

---
+++ src/components/KinoCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card text-center border-primary p-3">
+    <button
+      type="button"
+      class="close card-header text-right"
+      aria-label="Close"
+    >
+      <span aria-hidden="true">&times;</span>
+    </button>
+    <img
+      class="card-img-top"
+      src="@/assets/300x450.jpeg"
+      alt="Card image cap"
+    />
+    <div class="card-body">
+      <a href="#" class="btn btn-secondary w-100">Добавить</a>
+      <input class="mt-3 w-100" placeholder="url" />
+      <input class="mt-3 w-100" placeholder="описание" />
+    </div>
+  </div>
+</template>
+<button type="button" class="btn btn-primary">Primary</button>
+<script>
+export default {
+  name: "KinoCard",
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  max-width: 250px;
+  & .card-img-top {
+    height: 100px;
+  }
+
+}
+</style>
--- src/views/Banners.vue
+++ src/views/Banners.vue
@@ -1,10 +1,85 @@
 <template>
   <div class="about">
-    <h1>Banners page</h1>
+    <div class="card">
+      <div class="card-header text-center font-weight-bold">
+        На главной верх
+      </div>
+      <div class="row p-3">
+        <div class="col-md-6 text-muted">Pазмер 1000x190</div>
+        <div class="col-md-6 text-right">
+          <input
+            v-model="mainTopIsOn"
+            type="checkbox"
+            aria-label="Checkbox for on/off"
+          />
+        </div>
+      </div>
+
+      <div class="card-group p-3">
+        <KinoCard />
+        <KinoCard />
+        <KinoCard />
+        <KinoCard />
+      </div>
+
+      <div class="row p-3">
+        <div class="col-md-6">
+          Скорость вращения
+          <input v-model="mainTopRotationSpeed" type="text" class="w-25" />
+          <button
+            class="btn btn-outline-secondary dropdown-toggle"
+            type="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            сек
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">5</a>
+            <a class="dropdown-item" href="#">10</a>
+            <a class="dropdown-item" href="#">15</a>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <button type="button" class="btn btn-primary text-center">
+            Сохранить
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
+import KinoCard from "@/components/KinoCard.vue";
+
 export default {
   name: "Banners",
+  components: {
+    KinoCard,
+  },
+  data: function () {
+    return {
+      mainTopIsOn: true,
+      mainTopRotationSpeed: 5,
+      banners: [],
+    };
+  },
+  watch: {
+    mainTopIsOn: function () {
+      console.log(this.mainTopIsOn);
+      return;
+    },
+  },
+  mounted: {
+    // async fetchBanners() {}
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.card-group {
+  gap: 1rem;
+}
+</style>
