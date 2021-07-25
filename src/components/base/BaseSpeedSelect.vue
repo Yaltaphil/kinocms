390b782043736408622008ebdb02655199173417
Yaltaphil <yaltaphil@yandex.ru>
2021-07-25 12:54:24 +0300

    add: banner speed select

---
+++ src/components/base/BaseSpeedSelect.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="form-group d-flex align-items-start px-3">
+        <span>
+            <slot />
+        </span>
+        <div class="col-2">
+            <select
+                class="form-control shadow"
+                v-model="selected"
+                @change="$emit('change', $event.target.value)"
+            >
+                <option value="1" label="1 сек"></option>
+                <option value="3" label="3 сек"></option>
+                <option value="5" label="5 сек"></option>
+                <option value="10" label="10 сек"></option>
+                <option value="15" label="15 сек"></option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BaseSpeedSelect",
+
+    model: {
+        prop: "value",
+        event: "change",
+    },
+    props: {
+        selected: { type: Number, default: 5 },
+    },
+};
+</script>
