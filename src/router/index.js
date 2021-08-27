ca0d114fd8791d1aa4128ea2537b3d7f56832dec
Yaltaphil <yaltaphil@yandex.ru>
2021-08-27 12:01:44 +0300

    refactor: routes separated

--- src/router/index.js
+++ src/router/index.js
@@ -1,208 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import firebase from "firebase/app";
-import Home from "../views/admin/Home.vue";
 import Login from "../views/admin/Login.vue";
+
+import adminRoutes from "./adminRoutes";
+import siteRoutes from "./siteRoutes";
 
 Vue.use(VueRouter);
 
-const routes = [
-    {
-        path: "/admin/404",
-        alias: "*",
-        name: "notFound",
-        component: () =>
-            import(
-                /* webpackChunkName: "NotFound" */ "../views/admin/NotFound.vue"
-            ),
-    },
-
-    {
-        path: "/admin/banners",
-        name: "Banners",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "banners" */ "../views/admin/Banners.vue"
-            ),
-    },
-
-    {
-        path: "/admin/films",
-        name: "Films",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(/*webpackChunkName: "films" */ "../views/admin/Films.vue"),
-    },
-
-    {
-        path: "/admin/films/:filmIndex",
-        name: "FilmDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "films" */ "../views/admin/FilmDetails.vue"
-            ),
-    },
-
-    {
-        path: "/admin/cinemas",
-        name: "Cinemas",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "cinemas" */ "../views/admin/Cinemas.vue"
-            ),
-    },
-
-    {
-        path: "/admin/cinemas/:cinemaIndex",
-        name: "CinemaDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "cinemas" */ "../views/admin/CinemaDetails.vue"
-            ),
-    },
-
-    {
-        path: "/admin/cinemas/:cinemaIndex/:hallIndex",
-        name: "CinemaHallDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "cinemas" */ "../views/admin/CinemaHallDetails.vue"
-            ),
-    },
-
-    {
-        path: "/admin/news",
-        name: "News",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(/*webpackChunkName: "news" */ "../views/admin/News.vue"),
-    },
-
-    {
-        path: "/admin/news/:newsIndex",
-        name: "NewsDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "films" */ "../views/admin/NewsDetails.vue"
-            ),
-    },
-
-    {
-        path: "/admin/actions",
-        name: "Actions",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "actions" */ "../views/admin/Actions.vue"
-            ),
-    },
-
-    {
-        path: "/admin/actions/:actionIndex",
-        name: "ActionDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "actions" */ "../views/admin/ActionDetails.vue"
-            ),
-    },
-
-    {
-        path: "/admin/pages",
-        name: "Pages",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(/*webpackChunkName: "pages" */ "../views/admin/Pages.vue"),
-    },
-    {
-        path: "/admin/pages/:pageIndex",
-        name: "MainPageDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "pages" */ "../views/admin/MainPageDetails.vue"
-            ),
-    },
-    {
-        path: "/admin/pages/:pageIndex",
-        name: "ContactsPageDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "pages" */ "../views/admin/ContactsPageDetails.vue"
-            ),
-    },
-    {
-        path: "/admin/pages/:pageIndex",
-        name: "StandartPageDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "pages" */ "../views/admin/StandartPageDetails.vue"
-            ),
-    },
-
-    {
-        path: "/admin/users",
-        name: "Users",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(/*webpackChunkName: "users" */ "../views/admin/Users.vue"),
-    },
-
-    {
-        path: "/admin/users/:userIndex",
-        name: "UserDetails",
-        props: true,
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "users" */ "../views/admin/UserDetails.vue"
-            ),
-    },
-
-    {
-        path: "/admin/mailing",
-        name: "Mailing",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "mailing" */ "../views/admin/Mailing.vue"
-            ),
-    },
-
-    {
-        path: "/admin/mailing/choose",
-        name: "ChooseUsers",
-        meta: { layout: "admin", auth: true },
-        component: () =>
-            import(
-                /*webpackChunkName: "mailing" */ "../views/admin/ChooseUsers.vue"
-            ),
-    },
-
-    {
-        path: "/admin/home",
-        name: "Home",
-        component: Home,
-        meta: { layout: "admin", auth: true },
-    },
-
+const mainRoutes = [
     {
         path: "/admin",
         name: "Login",
@@ -218,10 +24,12 @@ const routes = [
     },
 ];
 
+const allRoutes = [...siteRoutes, ...adminRoutes, ...mainRoutes];
+
 const router = new VueRouter({
     mode: "history",
     base: process.env.BASE_URL,
-    routes,
+    routes: allRoutes,
 });
 
 router.beforeEach((to, from, next) => {
---
+++ src/router/siteRoutes.js
@@ -0,0 +1,3 @@
+const siteRoutes = [];
+
+export default siteRoutes;
